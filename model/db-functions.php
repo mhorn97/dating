cbd13afcb0b5e993051e8c7852a3a91c7df26c40
Michael Horn <michaelhorn1@hotmail.com>
2018-02-28 08:46:02 -0500

    Added comment of creation of database

---
+++ model/db-functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: micha
+ * Date: 2/28/2018
+ * Time: 5:42 AM
+ */
+/*
+ * CREATE TABLE IF NOT EXISTS members (
+    					member_id int NOT NULL AUTO_INCREMENT,
+                        fname varchar(255),
+                        lname varchar(255),
+                        age int,
+                        gender varchar(8),
+                        phone varchar(12),
+                        email varchar(255),
+                        state varchar(255),
+                        seeking varchar(8),
+                        bio varchar(255),
+                        premium tinyint,
+                        image varchar(255),
+                        interests varchar(255),
+                       PRIMARY KEY (member_id)
+						)ENGINE=InnoDB DEFAULT CHARSET=latin1
+ */
